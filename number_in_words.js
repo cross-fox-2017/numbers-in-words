960ad84aabfcc0142907d04196d313df3c90abeb
Samue PH Sinaga & Izhharuddin Syanmil <pair+samuelphsinaga.syanmil@devbootcamp.com>
2017-01-02 13:13:22 +0700

    release 0 done until ten

--- number_in_words.js
+++ number_in_words.js
@@ -1,6 +1,21 @@
 function in_words(angka) {
   // Your code here
+  var satuan= ["satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh"];
+  var number = [1,2,3,4,5,6,7,8,9,10];
+  var result = [];
+
+  for (var i = 0; i < satuan.length; i++){
+    if(angka == number[i]){
+      result.push(satuan[i])
+    }
+  }
+  return result
 }
 
 // Driver code
-console.log(in_words(1000000));
+// console.log(in_words(1000000));
+console.log(in_words(5));
+// console.log(in_words(27));
+// console.log(in_words(102));
+// console.log(in_words(38757));
+// console.log(in_words(22213717));
