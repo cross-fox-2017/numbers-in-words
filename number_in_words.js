df2eaa1222bdf3f7b7f585036933f9caefcd9bbb
Samue PH Sinaga & Izhharuddin Syanmil <pair+samuelphsinaga.syanmil@devbootcamp.com>
2017-01-02 14:22:33 +0700

    release 0 done until 999

--- number_in_words.js
+++ number_in_words.js
@@ -2,22 +2,23 @@ function in_words(angka) {
   // Your code here
   var panjang = angka.toString().length
   var satuan= ["", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan"];
-  var number = [0,1,2,3,4,5,6,7,8,9];
+  // var number = [0,1,2,3,4,5,6,7,8,9];
   var belasan = ["sepuluh", "sebelas", "dua belas", "tiga belas", "empat belas", "lima belas", "enam belas", "tujuh belas", "delapan belas", "sembilan belas"];
   var modifier = [" puluh", " ratus", " ribu"]
   var result = [];
-  for (var i = 0; i < panjang; i++){
-    if (angka%100 > 0 && angka > 200){
-      var x = angka/100
-      var xx = x.toFixed(0) -1
-      result.push(satuan[xx]);
+  // for (var i = 0; i < panjang; i++){
+    if (angka > 200){
+      var x = Math.floor(angka/100)
+      result.push(satuan[x]);
       result.push("ratus");
       angka = angka%100
+    } else if (angka > 99){
+      result.push("seratus")
+      angka -= 100
     }
-    if (angka%10 > 0 && angka>19){
-      var u = angka /10
-      var uu = u.toFixed(0) -1
-      result.push(satuan[uu])
+    if (angka>19){
+      var u = Math.floor(angka /10)
+      result.push(satuan[u])
       result.push("puluh")
       angka = angka %10
     } else if (angka>9 && angka <20){
@@ -29,7 +30,7 @@ function in_words(angka) {
       result.push(satuan[angka]);
       angka -= angka
     }
-  }
+  // }
   return result.join(" ")
 }
 // Driver code
@@ -37,7 +38,7 @@ function in_words(angka) {
 console.log(in_words(5));
 console.log(in_words(13));
 console.log(in_words(27));
-console.log(in_words(882));
+console.log(in_words(182));
 console.log(in_words(752));
 console.log(in_words(612));
 console.log(in_words(999));
